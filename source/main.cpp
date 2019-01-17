172116af03c3d1c6204cae4b374f342aa64f6fb1
jansorg <github@joachim-ansorg.de>
2019-01-17 21:49:56 +0100

    mac fixes

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QtWidgets/QMessageBox>
+
 #include "main_window.h"
 #include "gotime_tray_icon.h"
 
@@ -7,21 +8,22 @@ int main(int argc, char *argv[]) {
     QString command = "tom";
     bool bash = false;
 
+    #ifdef Q_OS_MAC
+    command = "./tom";
+    #endif
+
     if (argc >= 2) {
         command = QString(argv[1]);
     }
+
     if (argc == 3) {
         bash = QString(argv[2]) == "true";
     }
 
-    GotimeControl *control = new GotimeControl(command, bash, nullptr);
-
-    //  QApplication::setStyle("Fusion");
     QApplication::setQuitOnLastWindowClosed(false);
-
     QApplication app(argc, argv);
 
-    // we're hiding the main window by default, we only display the trayicon
+    GotimeControl *control = new GotimeControl(command, bash, nullptr);
     MainWindow mainWindow(control);
     mainWindow.show();
 

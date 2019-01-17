be3e3dccd184504c51574d4fb03da5f6837bff93
jansorg <github@joachim-ansorg.de>
2019-01-17 21:59:41 +0100

    deploy on mac

--- build-mac.sh
+++ build-mac.sh
@@ -1,1 +1,13 @@
-cmake .. -DCMAKE_PREFIX_PATH=/Users/jansorg/Qt/5.12.0/clang_64/
+#!/usr/bin/env bash
+
+go get github.com/jansorg/tom
+
+QTDIR="$HOME/Qt/5.12.0/clang_64/"
+
+rm -rf build
+mkdir build
+cd build
+cmake .. -DCMAKE_PREFIX_PATH="$QTDIR"
+cp $(which tom) tom-ui.app/Contents/MacOS
+$QTDIR/macdeployqt tom-ui.app -dmg
+

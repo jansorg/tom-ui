afeca8cd905e65c09d4f1241e3a11621bac80b2d
jansorg <github@joachim-ansorg.de>
2019-01-18 09:13:11 +0100

    update icons

---
+++ update-assets.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+inkscape -e res/AppIcon32.png -w 32 -h 32 res/AppIcon.svg
+inkscape -e res/AppIcon128.png -w 128 -h 128 res/AppIcon.svg
+
+inkscape -e res/AppIcon16.png -w 16 -h 16 res/AppIcon.svg
+convert res/AppIcon16.png res/AppIcon.ico && rm res/AppIcon16.png
+
+# create Apple Icons
+for s in 16 32 48 128 256;  do
+    inkscape -e res/AppIcon_${s}px.png -w $s -h $s res/AppIcon.svg
+done
+png2icns res/AppIcon.icns res/AppIcon_*px.png
+rm res/AppIcon_*px.png

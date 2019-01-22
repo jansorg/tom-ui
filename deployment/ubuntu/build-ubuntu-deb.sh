d41b9c9756f7249ee33c151473aebba302cedbf2
jansorg <github@joachim-ansorg.de>
2019-01-22 20:38:40 +0100

    deb packaging

--- deployment/ubuntu/build-ubuntu-deb.sh
+++ deployment/ubuntu/build-ubuntu-deb.sh
@@ -7,7 +7,7 @@ cd $HOME
 go get github.com/jansorg/tom
 git clone https://github.com/jansorg/tom-ui
 cd tom-ui
-git checkout "tag/$TOM_VERSION"
+git checkout "tag/v${TOM_VERSION}"
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE="Release" ..
--- release.bash
+++ release.bash
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 SOURCE="$PWD"
 VERSION="$1"
 [[ -z "$VERSION" ]] && echo "No version given" && exit -1
@@ -12,6 +10,15 @@ export TOM_VERSION="$VERSION"
 
 TARGET="$PWD/release-$VERSION"
 mkdir -p "$TARGET"
+
+function finish {
+    docker stop tom:ubuntu
+}
+
+trap finish EXIT
+
+set -e
+
 
 function buildUbuntu() {
     # ubuntu pakcages
@@ -24,14 +31,14 @@ function buildUbuntu() {
             DEB_TARGET="$TARGET/$UBUNTU_VERSION"
             mkdir -p "$DEB_TARGET"
 
-            CONTAINER="tom:$UBUNTU_VERSION"
+            CONTAINER="tom:ubuntu"
             true docker stop "$CONTAINER"
 
             docker build --rm -t "$CONTAINER" -f "$name" .
-            docker run -d -t -i --rm --name "tom-$UBUNTU_VERSION" -v "$DEB_TARGET:/dist" "$CONTAINER"
-            docker exec -e TOM_VERSION "tom-$UBUNTU_VERSION" bash /root/build-ubuntu-deb.sh
+            docker run -d -t -i --rm --name "tom-ubuntu" -v "$DEB_TARGET:/dist" "$CONTAINER"
+            docker exec -e TOM_VERSION "tom-ubuntu" bash /root/build-ubuntu-deb.sh
 
-            docker stop "$CONTAINER"
+            docker stop "tom-ubuntu"
 
             mv "$DEB_TARGET"/*.deb "$TARGET"
         )

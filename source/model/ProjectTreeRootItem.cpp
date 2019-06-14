851d3c8a980f546fc96060590ecec371e6b26232
jansorg <github@joachim-ansorg.de>
2019-06-14 21:41:11 +0200

    fix: show totals for year and yesterday for root project

--- source/model/ProjectTreeRootItem.cpp
+++ source/model/ProjectTreeRootItem.cpp
@@ -10,12 +10,16 @@ QVariant ProjectTreeRootItem::data(int column) const {
     switch (column) {
         case COL_NAME:
             return QApplication::tr("All projects");
+        case COL_YESTERDAY:
+            return _statusManager->getOverallStatus().yesterdayTotal.formatShort();
         case COL_TODAY:
             return _statusManager->getOverallStatus().dayTotal.formatShort();
         case COL_WEEK:
             return _statusManager->getOverallStatus().weekTotal.formatShort();
         case COL_MONTH:
             return _statusManager->getOverallStatus().monthTotal.formatShort();
+        case COL_YEAR:
+            return _statusManager->getOverallStatus().yearTotal.formatShort();
         case COL_TOTAL:
             return _statusManager->getOverallStatus().allTotal.formatShort();
         default:

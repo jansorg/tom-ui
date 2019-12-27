fa2d3ff99bb71bd3ffaf6dda324bf3e303c42c90
jansorg <github@joachim-ansorg.de>
2019-12-27 18:27:32 +0100

    fix build on macOS

--- deployment/build-mac-dmg.sh
+++ deployment/build-mac-dmg.sh
@@ -12,7 +12,7 @@ git pull
     cd "$HOME/tom/"
     git pull
     [[ -n "$TOM_VERSION" ]] && git checkout "v${TOM_VERSION}"
-    go build .
+    go build -o tom -ldflags "-s -w" .
 )
 
 QTDIR="$HOME/Qt/5.12.6/clang_64"
@@ -24,6 +24,6 @@ cmake -DCMAKE_PREFIX_PATH="$QTDIR" -DCMAKE_BUILD_TYPE="Release" "$FLAGS" ..
 make -j4
 
 mv tom-ui.app Tom.app
-go build -o Tom.app/Contents/MacOS/tom -ldflags "-s -w" github.com/jansorg/tom
+cp "$HOME/tom/tom" Tom.app/Contents/MacOS/tom
 install_name_tool -add_rpath "@executable_path/../Frameworks" Tom.app/Contents/MacOS/tom-ui
 "$QTDIR/bin/macdeployqt" Tom.app -dmg

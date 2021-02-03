a53102b1689ace7725b7a07ea0c4ff3d19be1586
jansorg <github@joachim-ansorg.de>
2021-02-03 14:00:01 +0100

    Signing and notarization for macOS app and dmg

--- deployment/build-mac-dmg.sh
+++ deployment/build-mac-dmg.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 set -e
+DIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
 
 FLAGS="$1"
+QTDIR="$HOME/Qt/5.15.2/clang_64"
+APPLE_DEV_ID="mail@ja-dev.eu"
+APPLE_DEV_PASS="$(cat ./apple-account-password.txt)"
+# 2nd Apple Certificate, made for Tom Time Tracker
+APPLE_CERT_ID="F5E7309AFF297C4E9563DD1FFBE3C865D6164EAA"
 
-git pull
-[[ -n "$TOM_VERSION" ]] && git checkout "v${TOM_VERSION}"
+[[ -n "$TOM_VERSION" ]] && git pull && git checkout "v${TOM_VERSION}"
 
 (
     rm -rf "$HOME/tom/"
     git clone "https://github.com/jansorg/tom.git" "$HOME/tom"
     cd "$HOME/tom/"
-    git pull
     [[ -n "$TOM_VERSION" ]] && git checkout "v${TOM_VERSION}"
-    go build -o tom -ldflags "-s -w" .
+    GO111MODULE=on go build -o tom -ldflags "-s -w" .
+    codesign -f -s "$APPLE_CERT_ID" tom
 )
 
-QTDIR="$HOME/Qt/5.15.2/clang_64"
+cd "$DIR/.."
 
 rm -rf build
 mkdir build
@@ -26,4 +31,32 @@ make -j4
 mv tom-ui.app Tom.app
 cp "$HOME/tom/tom" Tom.app/Contents/MacOS/tom
 install_name_tool -add_rpath "@executable_path/../Frameworks" Tom.app/Contents/MacOS/tom-ui
-"$QTDIR/bin/macdeployqt" Tom.app -dmg -timestamp -sign-for-notarization=
+
+"$QTDIR/bin/macdeployqt" Tom.app -dmg -always-overwrite -timestamp -codesign="$APPLE_CERT_ID" -sign-for-notarization="$APPLE_CERT_ID"
+
+# notarization, based on https://forum.qt.io/topic/96652/how-to-notarize-qt-application-on-macos/18
+# https://stackoverflow.com/questions/5158942/codesign-collisions-between-developer-and-enterprise-distribution
+echo "--> Start Notarization process"
+response=$(xcrun altool -t osx -f Tom.dmg --primary-bundle-id dev.tomtime.tom --notarize-app -u "${APPLE_DEV_ID}" -p "${APPLE_DEV_PASS}")
+requestUUID=$(echo "${response}" | tr ' ' '\n' | tail -1)
+
+while true; do
+  echo "--> Checking notarization status"
+
+  statusCheckResponse=$(xcrun altool --notarization-info "${requestUUID}" -u "${APPLE_DEV_ID}" -p "${APPLE_DEV_PASS}")
+  isSuccess=$(echo "${statusCheckResponse}" | grep "success")
+  isFailure=$(echo "${statusCheckResponse}" | grep "invalid")
+
+  if [[ "${isSuccess}" != "" ]]; then
+      echo "Notarization done!"
+      xcrun stapler staple -v Tom.dmg
+      echo "Stapler done!"
+      break
+  fi
+  if [[ "${isFailure}" != "" ]]; then
+      echo "Notarization failed"
+      return 1
+  fi
+  echo "Notarization not finished yet, sleep 1m then check again..."
+  sleep 60
+done
